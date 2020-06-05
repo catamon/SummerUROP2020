dfc53ce43ad3713737701bd5ece1c54e620380eb
Catalina Laptop <monriguezcata@gmail.com>
2020-06-05 11:50:19 -0500

    color added
    The tutorial is pretty similar to the one of Processing, so I just added some color related code

--- Tutorials/p5/Foamy_sunfish_2020_06_05_16_35_24/sketch.js
+++ Tutorials/p5/Foamy_sunfish_2020_06_05_16_35_24/sketch.js
@@ -4,10 +4,13 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    fill(0,0,0,128);
   }
   else {
     fill(255,255,0);
+  }
+  if (keyIsPressed) {
+    noFill();
   }
   ellipse(mouseX,mouseY,80,80);
 }
