3af1f0192e7ba6d0fd49429a5cbd9d099d8b08d9
Catalina Laptop <monriguezcata@gmail.com>
2020-06-05 11:54:08 -0500

    shapes code
    added code to add shapes, tutorial really similar to processing

--- Tutorials/p5/Foamy_sunfish_2020_06_05_16_35_24/sketch.js
+++ Tutorials/p5/Foamy_sunfish_2020_06_05_16_35_24/sketch.js
@@ -5,12 +5,16 @@ function setup() {
 function draw() {
   if (mouseIsPressed) {
     fill(0,0,0,128);
+    ellipse(mouseX,mouseY,80,80);
   }
   else {
     fill(255,255,0);
+
+    if (keyIsPressed) {
+      noFill();
+    }
+    rectMode(CENTER);
+    rect(mouseX,mouseY,80,80);
   }
-  if (keyIsPressed) {
-    noFill();
-  }
-  ellipse(mouseX,mouseY,80,80);
+
 }
